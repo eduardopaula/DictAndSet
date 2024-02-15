f343c1873e8976b333071e159c8d9bd0b7dfaa87
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-15 14:12:36 -0300

    Aula 192

--- buy_computer_dict.py
+++ buy_computer_dict.py
@@ -1,19 +1,29 @@
-avaiable_parts = {"1": "computer",
-                  "2": "monitor",
-                  "3": "keyboard",
-                  "4": "mouse",
-                  "5": "hdmi cable",
-                  "6": "dvd drive",
-                  }
+available_parts = {"1": "computer",
+                   "2": "monitor",
+                   "3": "keyboard",
+                   "4": "mouse",
+                   "5": "hdmi cable",
+                   "6": "dvd drive",
+                   }
 
 current_choice = None
+computer_parts = {}     # create an empty dictionary
+
 while current_choice != "0":
-    if current_choice in avaiable_parts:
-        chosen_part = avaiable_parts[current_choice]
-        print("Adding {}".format(chosen_part))
+    if current_choice in available_parts:
+        chosen_part = available_parts[current_choice]
+        if current_choice in computer_parts:
+            # it's already in, so remove it
+            print(f"Removing {chosen_part}")
+            computer_parts.pop(current_choice)
+        else:
+            print(f"Adding {chosen_part}")
+            computer_parts[current_choice] = chosen_part
+        print(f"Your dictionary now contains: {computer_parts}")
     else:
-        for key, values in avaiable_parts.items():
-            print(key, values, sep=": ")
+        print("Please add options from the list")
+        for key, value in available_parts.items():
+            print(f"{key}: {value}")
         print("0: to finish")
 
     current_choice = input("> ")
---
+++ meal_planner.py
@@ -0,0 +1,18 @@
+from contents import pantry, recipes
+
+# display_dict = {str(index + 1): meal for index, meal in enumerate(recipes)}
+display_dict = {}
+for index, key in enumerate(recipes):
+    display_dict[str(index + 1)] = key
+
+while True:
+    # Display a menu of the recipes we know how to cook
+    print("Please choose your recipe")
+    print("-------------------------")
+    for key, value in display_dict.items():
+        print(f"{key} - {value}")
+
+    choice = input(": ")
+
+    if choice == "0":
+        break
