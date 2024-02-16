604c687aaca013c0cf64e22c290daf755e24bfd2
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-16 09:45:12 -0300

    Aula 196

--- meal_planner.py
+++ meal_planner.py
@@ -22,8 +22,11 @@ while True:
         print("checking ingredients ...")
         ingredients = recipes[selected_item]
         print(ingredients)
-        for food_item in ingredients:
-            if food_item in pantry:
+        for food_item, required_quantity in ingredients.items():
+            quantity_in_pantry = pantry.get(food_item, 0)
+            if required_quantity <= quantity_in_pantry:
                 print(f"\t{food_item}: OK")
             else:
-                print(f"\tYou don't have a necessary ingredient: {food_item}")
+                quantity_to_buy = required_quantity - quantity_in_pantry
+                print(f"\tYou need to buy {quantity_to_buy} of {food_item}")
+                # print(f"\tYou don't have a necessary ingredient: {food_item}")
