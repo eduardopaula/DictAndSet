6d17e31982862821ccf9c7c5e9e5bed773d39b4a
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-16 10:33:53 -0300

    Aula 200

--- dict_defaults.py
+++ dict_defaults.py
@@ -3,7 +3,7 @@ from contents import pantry
 chicken_quantity = pantry.setdefault("chicken", 0)
 print(f"chicken: {chicken_quantity}")
 
-beans_quantity = pantry.setdefault("beans", 10)
+beans_quantity = pantry.setdefault("beans", 0)
 print(f"beans: {beans_quantity}")
 
 ketchup_quantity = pantry.get("ketchup", 0)
