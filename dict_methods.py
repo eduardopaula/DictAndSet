e1817d94fc03ba0e492e4bbd840a056bb38aea75
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-19 10:34:33 -0300

    Aula 206

--- dict_methods.py
+++ dict_methods.py
@@ -9,26 +9,50 @@ d = {
     7: "seven",
     8: "eight",
     9: "nine",
+    "iv": "four",
 }
 
 pantry_items = ['chicken', 'spam', 'egg', 'bread', 'lemon']
 
-d2 = {
-    7:  "lucky seven",
-    10: "ten",
-    3:  "this is the new three",
-}
+v = d.values()
+print(v)
 
-d.update(d2)
-for key, value in d.items():
-    print(key, value)
+d[10] = "ten"
+print(v)
+
+print("four" in v)
+print("eleven" in v)
+
+keys = list(d.keys())
+values = list(v)    # => list(d.values())
+if "four" in values:
+    index = values.index("four")
+    key = keys[index]
+    print(f"{d[key]} was found with the key {key}")
 
 print()
 
-d.update(enumerate(pantry_items))
 for key, value in d.items():
-    print(key, value)
+    if value == "four":
+        print(f"{d[key]} was found with the key {key}")
 
+# Code for "The dict `update` method" lecture.
+# d2 = {
+#     7:  "lucky seven",
+#     10: "ten",
+#     3:  "this is the new three",
+# }
+#
+# d.update(d2)
+# for key, value in d.items():
+#     print(key, value)
+#
+# print()
+#
+# d.update(enumerate(pantry_items))
+# for key, value in d.items():
+#     print(key, value)
+#
 # Code for "The remaining `dict` methods" lecture.
 # ************************************************
 # new_dict = dict.fromkeys(pantry_items, 0)
